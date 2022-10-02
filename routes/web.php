00845990b4d551dac19721cfa476650bb597271d
Sajidul Haque <sajidulhaque007@gmail.com>
2022-10-03 02:15:39 +0600

    Updated routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,14 @@ Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->name('admi
 Route::get('vendor/dashboard', [VendorController::class, 'dashboard'])->name('vendor.dashboard');
 Route::post('user/login', [AuthController::class, 'login'])->name('login'); 
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-
+//User
 Route::post('/unique_email',[AuthController::class, 'unique_email'])->name('unique_email');
 Route::get('/users', [AdminController::class, 'index']);
 Route::post('/adduser',[AdminController::class, 'addUser'])->name('adduser');
 Route::get('edit-user/{id}', [AdminController::class, 'editUser'])->name('edit-user');
 Route::put('update-user/{id}', [AdminController::class, 'updateUser'])->name('update-user');
 Route::get('delete-user/{id}', [AdminController::class, 'deleteUser'])->name('delete-user');
-
+//Vendor
 Route::get('/vendors',[AdminController::class, 'vendor'])->name('vendor');
 Route::post('/addvendor',[AdminController::class, 'addVendor'])->name('add-vendor');
 Route::get('/edit-vendor/{id}',[AdminController::class, 'editVendor'])->name('edit-vendor');
